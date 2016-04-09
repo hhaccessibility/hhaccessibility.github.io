3e6b995b880ca2f61496bfe68cf743afc77de14f
amrit singh bains <amrit.singh.bains@live.com>
2016-04-09 19:12:33 -0400

    report5

--- node2.js
+++ node2.js
@@ -428,7 +428,7 @@ app.post('/showstudents', function (req, res) {
     display += " AND ";
     display += req.body.country == "all"?"(country like '%')":"(country = '" + req.body.country + "')";
     display += " AND ";
-    display += req.body.semesterregistered == "all"?"(semesterregistered like '%')":"(semesterregistered = " + req.body.semesterregistered + ")";
+    display += req.body.semesterregistered == "all"?"(semesterregistered like '%')":"(semesterregistered = '" + req.body.semesterregistered + "')";
     display += " AND ";
     display += req.body.internshipstatus == "all"?"(internshipstatus like '%')":"(internshipstatus = '" + req.body.internshipstatus + "')";
 
