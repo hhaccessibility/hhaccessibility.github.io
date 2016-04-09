98ea473a54dd4fbfaa6b482491471eaa8ea9fefc
amrit singh bains <amrit.singh.bains@live.com>
2016-04-09 18:29:34 -0400

    studentids13

--- node2.js
+++ node2.js
@@ -490,7 +490,7 @@ app.post('/showstudents', function (req, res) {
 });
 
 function showStudents(studentids, res){
-    var queryString = "select login.photoid, "+
+    var queryString = "select login.photoid, student.id, "+
         "student.firstname, student.internshipstatus,"+
         "student.lastname, student.residentstatus, student.country, student.gender, "+
         "student.studentid from login inner join student on "+
