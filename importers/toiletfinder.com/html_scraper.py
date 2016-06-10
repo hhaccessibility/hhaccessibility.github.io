a7943390bd2edeccc44f2ffad424c8548321782b
Josh Greig <jgreig@nextdimensioninc.com>
2016-06-10 11:27:12 -0400

    importers: included thumbnail_url and venue_url in the scraped data
    Related to issue-1

--- importers/toiletfinder.com/html_scraper.py
+++ importers/toiletfinder.com/html_scraper.py
@@ -39,24 +39,39 @@ def scrape_toilets(html_filename):
 			locality = get_text_from_css(toilet_element, '.locality')
 			phone_number = get_text_from_css(toilet_element, '.views-field-field-phone-number')
 			toilet_paper_type = get_text_from_css(toilet_element, '.views-field-field-tp-availability a')
-			
+			thumbnail_element = toilet_element.cssselect('.views-field-field-image img')
+			thumbnail_url = ''
+			if thumbnail_element:
+				thumbnail_element = thumbnail_element[0]
+				thumbnail_url = thumbnail_element.get('src')
+
+			venue_url = ''
+			venue_element = toilet_element.cssselect('.views-field-title a')
+			if venue_element:
+				venue_element = venue_element[0]
+				venue_url = venue_element.get('href')
+
 			# add new Toilet to result list.
 			result.append(Toilet(name, total_logged_dumps, street_address, postal_code, 
-				locality, country, phone_number, toilet_paper_type))
+				locality, country, phone_number, toilet_paper_type, thumbnail_url, 
+				venue_url))
+
 	return result
 
 def scrape_all_html(print_progress):
 	"""
 	Processes all HTML files in the raw_html directory and returns a list of Toilet objects to represent the scraped information
 	"""
-	result = []
+	result = set([])
 	raw_html_dir = 'raw_html'
+	count = 0
 	# loop through all files with the .html file extension.
 	for filename in os.listdir(raw_html_dir):
-		if print_progress:
-			print 'Processing file ' + filename
 		if filename.endswith(".html"):	
-			result = result + scrape_toilets(raw_html_dir + '/' + filename)
+			if print_progress and (count % 100 == 0):
+				print 'Processing file ' + str(count) + ': ' + filename
+			count = count + 1
+			result |= set(scrape_toilets(raw_html_dir + '/' + filename))
 
 	return result
 
