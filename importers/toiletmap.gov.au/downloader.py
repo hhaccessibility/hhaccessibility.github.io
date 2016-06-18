b16dd1133f52f22312f749e9cb5451b369ff6d4d
Josh Greig <jgreig@nextdimensioninc.com>
2016-06-18 06:01:29 -0400

    importers: added code to downloader.py for toiletmap.gov.au
    Related to issue-8

--- importers/toiletmap.gov.au/downloader.py
+++ importers/toiletmap.gov.au/downloader.py
@@ -8,19 +8,34 @@ longitude_range = 8
 
 output_dir = 'raw_data'
 
-def download_data(latitude, longitude, skip_if_downloaded):
+def download_toilet_details(toilet_id, skip_if_downloaded):
+	filename = output_dir + '/toilet_' + str(toilet_id) + '.html'
+	if os.path.isfile(filename) and skip_if_downloaded:
+		return
+
+	url = 'https://toiletmap.gov.au/Toilet/' + str(toilet_id)
+	try:
+		response = urllib2.urlopen(url)
+		data = response.read()
+	except urllib2.HTTPError, e:
+		data = "Could not download: " + url + ", " + str(e) + ", code: " + str(e.code)
+
+	with open(filename, 'wb') as f:
+		f.write(data)
+
+def download_rectangle(latitude, longitude, skip_if_downloaded):
 	lat2 = latitude + lat_range
 	long2 = longitude + longitude_range
 	filename = output_dir + '/data_' + str(latitude) + '_' + str(longitude) + '.json'
-	if os.path.isfile(filename):
+	if os.path.isfile(filename) and skip_if_downloaded:
 		return
-	url = 'https://toiletmap.gov.au/api/MapAPI/GetMapDetails?areaId=&swLat=' + str(latitude) + '&swLon=' + str(longitude) + '&neLat=' + str(lat2) + '&neLon=' + str(long2) + '&zoom=7&address=Sydney%2C+New+South+Wales%2C+Australia&searchmask=63&IsOpenNow=&clientTime=Thu%2C+16+Jun+2016+01%3A12%3A39+GMT'
+	url = 'https://toiletmap.gov.au/api/MapAPI/GetMapDetails?areaId=&swLat=' + str(latitude) + '&swLon=' + str(longitude) + '&neLat=' + str(lat2) + '&neLon=' + str(long2) + '&zoom=7&address=Sydney%2C+New+South+Wales%2C+Australia&searchmask=63&IsOpenNow=&clientTime=Thu%2C+17+Jun+2016+01%3A12%3A39+GMT'
 	response = urllib2.urlopen(url)
 	data = response.read()
 	with open(filename, 'wb') as f:
 		f.write(data)
 
-def download_all(skip_if_downloaded, printing_progress):
+def download_rectangles(skip_if_downloaded, printing_progress):
 	min_latitude = -45
 	max_latitude = -10
 	min_longitude = 110
@@ -39,8 +54,20 @@ def download_all(skip_if_downloaded, printing_progress):
 			print 'processing file ' + str(files_processed) + ' of ' + str(total_num_files)
 
 		for longitude in range(min_longitude, max_longitude, longitude_range):
-			download_data(latitude, longitude, skip_if_downloaded)
+			download_rectangle(latitude, longitude, skip_if_downloaded)
 			files_processed += 1
+
+def download_all(skip_if_downloaded, printing_progress):
+	download_rectangles(skip_if_downloaded, printing_progress)
+	count = 0
+	max_toilet_id = 50000
+
+	for toilet_id in range(1, max_toilet_id):
+		if count % 5 == 0 and printing_progress:
+			print 'toilet ' + str(toilet_id) + ' of ' + str(max_toilet_id)
+
+		count += 1
+		download_toilet_details(toilet_id, skip_if_downloaded)
 
 if __name__ == '__main__':
 	download_all(True, True)
