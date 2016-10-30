da95205c8f05529b74c31b196354ba4438a1afa6
jgreig <josh.greig@gmail.com>
2016-10-30 02:59:53 -0400

    app: made migration for a few initial database tables
    Related to #34 or issue-34.

--- app/config/database.php
+++ app/config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'hhaccessibility'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,12 +52,12 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
+        'hhaccessibility' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'hhaccessibility'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
---
+++ app/database/migrations/2016_10_30_053317_create_initial_database_schema.php
@@ -0,0 +1,85 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInitialDatabaseSchema extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username', 100)->unique();
+            $table->string('password_hash', 32);
+        });
+        Schema::create('role', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100)->unique();
+            $table->string('description', 255);
+        });
+        Schema::create('user_role', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('user');
+            $table->integer('role_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('role');
+			$table->unique(array('role_id', 'user_id'));
+        });
+        Schema::create('question_category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 255);
+        });
+        Schema::create('question', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('question_text');
+		    $table->integer('question_category_id')->nullable();
+			$table->foreign('question_category_id')->references('id')->on('question_category');
+        });
+        Schema::create('building_group', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 255);
+        });
+        Schema::create('building', function (Blueprint $table) {
+            $table->increments('id');
+ 		    $table->integer('building_group_id')->nullable();
+			$table->string('name', 255)->nullable();
+			$table->float('longitude');
+			$table->float('latitude');
+        });
+        Schema::create('user_answer', function (Blueprint $table) {
+            $table->increments('id');
+ 		    $table->integer('answered_by_user_id')->unsigned();
+			$table->foreign('answered_by_user_id')->references('id')->on('user');
+    	    $table->integer('question_id')->unsigned();
+			$table->foreign('question_id')->references('id')->on('question');
+    	    $table->integer('building_id')->unsigned();
+			$table->foreign('building_id')->references('id')->on('building');
+    		$table->string('answer_value', 255);
+			$table->datetime('when_submitted');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_answer');
+        Schema::dropIfExists('building');
+        Schema::dropIfExists('building_group');
+        Schema::dropIfExists('question');
+        Schema::dropIfExists('question_category');
+        Schema::dropIfExists('user_role');
+        Schema::dropIfExists('user');
+        Schema::dropIfExists('role');
+    }
+}
