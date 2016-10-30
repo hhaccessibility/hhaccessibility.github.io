40e54ed723fbb26b1c49b4df523fc21cf62681d0
jgreig <josh.greig@gmail.com>
2016-10-30 14:33:57 -0400

    app: seeded some data for the hhaccessibility database

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -2,8 +2,21 @@
 
 use Illuminate\Database\Seeder;
 
+function object_to_array($obj) {
+	return (array)$obj;
+}
+
 class DatabaseSeeder extends Seeder
 {
+		
+	public static function readTableData($json_filename) {
+		$content = file_get_contents('database/seeds/data/'.$json_filename);
+		$content = file_get_contents('database/seeds/data/'.$json_filename);
+		$content = json_decode($content);
+		$content = array_map('object_to_array', $content);
+		return $content;
+	}
+		
     /**
      * Run the database seeds.
      *
@@ -11,6 +24,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+		$tables_to_seed_using_json = ['role', 'building', 'building_group', 'question', 'question_category'];
+		foreach ($tables_to_seed_using_json as $table_name) {
+			DB::table($table_name)->delete();
+		}
+		
+		foreach (array_reverse($tables_to_seed_using_json) as $table_name) {
+			DB::table($table_name)->insert(DatabaseSeeder::readTableData($table_name . '.json'));
+		}
     }
 }
