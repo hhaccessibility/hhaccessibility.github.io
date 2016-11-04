679717b3ddd249389722f4d8c723e74fdc44097e
jgreig <josh.greig@gmail.com>
2016-11-03 20:53:43 -0400

    database config: replaced localhost with 127.0.0.1 to make it easier for new developers to get the web app working locally
    This is related to a problem that Jinghao Li ran into when he was trying
    to get the migrate to work.
    
    This is also related to a suggestion found at:
    http://stackoverflow.com/questions/20723803/pdoexception-sqlstatehy000-2002-no-such-file-or-directory

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return [
 
         'hhaccessibility' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'hhaccessibility'),
             'username' => env('DB_USERNAME', 'root'),
