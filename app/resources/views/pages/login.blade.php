5f9e42e9d061ed17a11e43315ba32c3142287038
jgreig <josh.greig@gmail.com>
2016-11-12 10:32:14 -0500

    app: login: centred the login form and removed some ugly duplication of markup

--- app/resources/views/pages/login.blade.php
+++ app/resources/views/pages/login.blade.php
@@ -1,37 +1,46 @@
 @extends('layouts.default')
 @section('content')
 <h1>Sign In</h1>
-{{ Form::open(array('url' => 'login')) }}
+<div class="login-form">
+	{{ Form::open(array('url' => 'login')) }}
 
-<!-- if there are login errors, show them here -->
-<p>
-    {{ $errors->first('username') }}
-    {{ $errors->first('password') }}
-</p>
+	<!-- if there are login errors, show them here -->
+	@if (!$errors->isEmpty())
+	<ul>
+		@if ($errors->first('username'))
+		<li>{{ $errors->first('username') }}</li>
+		@endif
+		@if ($errors->first('password'))
+		<li>{{ $errors->first('password') }}</li>
+		@endif
+	</ul>
+	@endif
 
-<p>
-    {{ Form::label('username', 'Username') }}
-    {{ Form::text('username', Input::old('username'), array('placeholder' => 'test')) }}
-</p>
+	<div class="row">
+		<div class="col-xs-4">
+			{{ Form::label('username', 'Username') }}
+		</div>
+		<div class="col-xs-8">
+			{{ Form::text('username', Input::old('username'), array('placeholder' => 'test')) }}
+		</div>
+	</div>
 
-<p>
-    {{ Form::label('password', 'Password') }}
-    {{ Form::password('password') }}
-</p>
+	<div class="row">
+		<div class="col-xs-4">
+			{{ Form::label('password', 'Password') }}
+		</div>
+		<div class="col-xs-8">
+			{{ Form::password('password') }}
+		</div>
+	</div>
 
-<p>{{ Form::submit('Submit!') }}</p>
-{{ Form::close() }}
-<div class="form-group">
-	<label class="control-label">Username</label>
-	<input type="text">
-</div>
-<div class="form-group">
-	<label class="control-label">Password</label>
-	<input type="password">
-</div>
-<div class="form-group">
-	<label class="control-label">Remember my login</label>
-	<input type="checkbox">
+	<p>{{ Form::submit('Submit!') }}</p>
+	{{ Form::close() }}
+
+	<div class="form-group">
+		<label class="control-label">Remember my login</label>
+		<input type="checkbox">
+	</div>
 </div>
 
 <h4>Don't have an account?</h4>
