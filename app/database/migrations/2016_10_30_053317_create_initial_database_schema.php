d496260d6d22f5c28b33c62bdc20be5c3a2dae19
JGreig <josh.greig@gmail.com>
2016-11-15 07:39:45 -0500

    app: added email, first_name, and last_name fields to the user table

--- app/database/migrations/2016_10_30_053317_create_initial_database_schema.php
+++ app/database/migrations/2016_10_30_053317_create_initial_database_schema.php
@@ -16,6 +16,9 @@ class CreateInitialDatabaseSchema extends Migration
         Schema::create('user', function (Blueprint $table) {
 			$table->increments('id');
 			$table->string('username', 100)->unique();
+			$table->string('email', 255)->unique()->nullable();
+			$table->string('first_name', 255)->nullable();
+			$table->string('last_name', 255)->nullable();
 			$table->char('password_hash', 60);
 			$table->string('remember_token', 60)->nullable();
 			$table->double('search_radius_km', 11, 6)->nullable();
