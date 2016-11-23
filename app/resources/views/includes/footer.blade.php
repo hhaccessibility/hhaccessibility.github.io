e4a9eff5650e592aa40d2b649583f6d605d93015
JGreig <josh.greig@gmail.com>
2016-11-23 07:38:20 -0500

    app: fixed broken link to JMCC

--- app/resources/views/includes/footer.blade.php
+++ app/resources/views/includes/footer.blade.php
@@ -1,1 +1,1 @@
-<div id="copyright text-right">&copy; Copyright 2016 <a href="www.jmccentre.ca/">John McGivney Children's Centre</a></div>
+<div id="copyright text-right">&copy; Copyright 2016 <a href="http://www.jmccentre.ca/">John McGivney Children's Centre</a></div>
