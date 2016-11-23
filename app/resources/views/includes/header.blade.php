69070eb6b6905a0c518316f522f4655d97915407
jgreig <josh.greig@gmail.com>
2016-11-22 22:04:35 -0500

    renamed hhaccessibility to Access Locator and added titles to tags in the home page

--- app/resources/views/includes/header.blade.php
+++ app/resources/views/includes/header.blade.php
@@ -1,6 +1,6 @@
 <div class="navbar">
     <div class="navbar-inner">
-        <a id="logo" href="/">HHAccessibility</a>
+        <a id="logo" href="/">Access Locator</a>
         <ul class="nav">
             <li><a href="/">Home</a></li>
             <li><a href="/about">About</a></li>
