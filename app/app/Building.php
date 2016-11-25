0068e3dbf75d07dc3a50a0e68f2a1b0db847867d
jgreig <josh.greig@gmail.com>
2016-11-24 22:19:05 -0500

    app: added over new 300 buildings and a data_source table to help us track sources and react better if we run into a copyright complaint

--- app/app/Building.php
+++ app/app/Building.php
@@ -6,14 +6,14 @@ use Eloquent;
 class Building extends Eloquent
 {
     protected $fillable = [
-        'name', 'longitude', 'latitude', 'owner_user_id',
+        'name', 'longitude', 'latitude', 'owner_user_id', 'data_source_id',
     ];
 	public $timestamps = false;
 
 	protected $table = 'building';
 
     /**
-     * The tags that belong to a building.
+     * The tags that belong to this building.
      */
     public function tags()
     {
