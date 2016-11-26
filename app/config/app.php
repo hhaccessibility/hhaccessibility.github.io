2334d2b75db558d9c007fe8d34d11d1d979eaea9
jgreig <josh.greig@gmail.com>
2016-11-25 22:09:00 -0500

    app: turned debug on and improved the application name

--- app/config/app.php
+++ app/config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'HHAccessibility',
+    'name' => 'Access Locator',
 
     /*
     |--------------------------------------------------------------------------
@@ -38,7 +38,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
