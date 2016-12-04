5523d74c19df7e71654db3974716aae718fee78a
jgreig <josh.greig@gmail.com>
2016-12-04 18:01:12 -0500

    app: switched from arial to Didact Gothic font family
    Areeba suggested the font and it seemed like a simple improvement to try.

--- app/resources/views/includes/head.blade.php
+++ app/resources/views/includes/head.blade.php
@@ -6,6 +6,7 @@
 
 <!-- Fonts -->
 <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Didact+Gothic">
 <link href="/css/app.css" rel="stylesheet" type="text/css">
 <link href="/css/font-awesome-4.7.0/css/font-awesome.css" rel="stylesheet" type="text/css">
 <link rel="shortcut icon" href="{{{ asset('favicon.ico') }}}">
