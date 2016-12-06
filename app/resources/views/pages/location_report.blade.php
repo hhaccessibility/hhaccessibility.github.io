c171baf0a3a6ae6ca842d2ea16b3014d20ca76d2
jgreig <josh.greig@gmail.com>
2016-12-06 18:44:56 -0500

    app: added associated location tags to the location report

--- app/resources/views/pages/location_report.blade.php
+++ app/resources/views/pages/location_report.blade.php
@@ -3,7 +3,18 @@
 
 <div class="location-report">
 	<h1>{{ $location->name }}</h1>
-	<address>{{ $location->address }}</address>
+	<div class="row">
+		<div class="col-xs-6">
+			<address>{{ $location->address }}</address>
+		</div>
+		<div class="col-xs-6 text-right">
+			@foreach ( $location->tags()->orderBy('name')->get() as $location_tag )
+				<a class="location-tag" href="/search-by-tag/{{ $location_tag->id }}">
+					{{ $location_tag->name }}
+				</a>
+			@endforeach
+		</div>
+	</div>
 	<div class="questions">
 		@foreach ( $question_categories as $category )
 
