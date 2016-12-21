1340c70d2823a9c8fe5fc2e74ac41e9027e0bf7e
jgreig <josh.greig@gmail.com>
2016-12-21 06:18:11 -0500

    app: removed the Search link since it was just a blank page and not sketched yet
    Related to #125.

--- app/resources/views/includes/header.blade.php
+++ app/resources/views/includes/header.blade.php
@@ -4,7 +4,6 @@
 		<li><a href="/">Home</a></li>
 		<li><a href="/our-vision">Our Vision</a></li>
 		<li><a href="/profile">Profile</a></li>
-		<li><a href="/location-search">Search</a></li>
 		<li><a href="/contact">Contact</a></li>
 	</ul>
 </div>
