3945113da09782d0adc4c2c1b4f94f5c6a599799
jgreig <josh.greig@gmail.com>
2016-12-30 16:07:32 -0500

    app: added a few under development messages to better explain things for people who may be reviewing the demo application

--- app/resources/views/pages/review_thanks.blade.php
+++ app/resources/views/pages/review_thanks.blade.php
@@ -4,7 +4,8 @@
 <h1>Report on a specific building</h1>
 
 <p>
-Thank you for leaving a review.  We hope to see more reviews from you and reward you more points.
+Thank you for leaving a review.
+We hope to see more reviews from you and reward you more points.
 </p>
 
 @stop
