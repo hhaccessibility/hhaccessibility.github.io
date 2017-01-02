7bdaaf5dd11837230816f5946bca260cc42b5e43
jgreig <josh.greig@gmail.com>
2017-01-02 15:07:52 -0500

    ProfilePhotoUploadController improved to have a new size calculation function to eventually be used for resizing the uploaded image

--- app/app/Http/Controllers/ProfilePhotoUploadController.php
+++ app/app/Http/Controllers/ProfilePhotoUploadController.php
@@ -20,13 +20,36 @@ class ProfilePhotoUploadController extends Controller {
             return redirect()->intended('signin');
         }
     }
+
+	private static function getPhotoDimensionsFromUploadDimensions($width, $height)
+	{
+		$aspect_ratio = ($width * 1.0) / $height;
+		$max_height = 200;
+		$max_width = 730;
+		
+		if ( $aspect_ratio > ($max_width * 1.0) / $max_height )
+		{
+			if ( $width > $max_width )
+			{
+				// scale down the image to have a 730 pixel width.
+				$width = $max_width;
+				$height = round($max_width / $aspect_ratio);
+			}
+		}
+		else if ( $height > $max_height )
+		{
+			$height = $max_height;
+			$width = $max_height * $aspect_ratio;
+		}
+		return ['width' => $width, 'height' => $height];
+	}
 	
 	public function post(Request $request)
 	{
         if (BaseUser::isSignedIn())
         {
 			$validation_rules = array(
-				'profile_photo' => 'required|file|image|dimensions:min_width=10,min_height=10',
+				'profile_photo' => 'required|file|image|dimensions:min_width=32,min_height=32',
 			);
 			$validator = Validator::make(Input::all(), $validation_rules);
 			if ($validator->fails())
@@ -34,7 +57,11 @@ class ProfilePhotoUploadController extends Controller {
 				return Redirect::to('profile-photo-upload')->withErrors($validator)->withInput();	
 			}
 			
-			// FIXME: Write the file.
+			// FIXME: get dimensions from actual uploaded image.
+			$width = 1;
+			$height = 1;
+			$newDimensions = ProfilePhotoUploadController::getPhotoDimensionsFromUploadDimensions($width, $height);
+			// FIXME: Write the file to 'resources/assets/user_profile_images'.
 			
             return view('pages.profile.photo_upload_success');
 		}
