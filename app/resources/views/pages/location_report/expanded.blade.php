5eeca94d6573d6e6ba4031d71f02ea1b7a43604f
jgreig <josh.greig@gmail.com>
2017-01-26 20:52:11 -0500

    replaced all content on the expanded location report with Under Development
    Related to #173

--- app/resources/views/pages/location_report/expanded.blade.php
+++ app/resources/views/pages/location_report/expanded.blade.php
@@ -6,48 +6,8 @@
 
 <div class="location-report">
 
-	Under Development
+	<h1>Under Development</h1>
 
-	@include('pages.location_report.top', array(
-		'rating_system' => $rating_system,
-		'location' => $location,
-		'question_category_id' => $question_category_id))
-
-	@if ($rating_system === 'personal' && !$personal_rating_is_available)
-		@include('pages.location_report.personal_not_available')
-	@else
-		<div class="questions">
-			@foreach ( $question_categories as $category )
-				<div class="question-category">
-					<div class="title-bar">
-						<a href="/location-report/{{ $location->id }}/{{ $rating_system }}/{{ $category->id }}">
-							@include('pages.components.pie_graph', array('percent' => $category->getAccessibilityRating($location->id, $rating_system)))
-							<span class="question-category-name">{{ $category->name }}</span>
-						</a>
-					</div>
-					@if ($category->id === $question_category_id)
-						<ol>
-						@foreach ( $category->questions as $question )
-							<li>
-							{!! $question->question_html !!}
-							({{ $question->getAccessibilityRating($location->id, $rating_system).'%' }})
-							</li>
-						@endforeach
-						</ol>
-					@endif
-				</div>
-			@endforeach
-		</div>
-	@endif
-	<h2>Comments</h2>
-	<p>There are a total of {{ count($comments) }} comment(s) for this location.</p>
-	<div class="comments">
-	@foreach ( $comments as $comment )
-		<div>
-		{{ $comment->content }}
-		</div>
-	@endforeach
-	</div>
 </div>
 
 @stop
