c8837da20c822ee3017a87e9f25ea4f1b9551f1d
jgreig <josh.greig@gmail.com>
2017-01-31 21:34:55 -0500

    app: made password recovery validate and send an email

--- app/app/Http/Controllers/PasswordRecoveryController.php
+++ app/app/Http/Controllers/PasswordRecoveryController.php
@@ -1,14 +1,39 @@
 <?php namespace App\Http\Controllers;
 
 use App\BaseUser;
+use App\User;
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Redirect;
 
 class PasswordRecoveryController extends Controller {
 
 	public function form(Request $request)
 	{
-		return view('pages.password_recovery');
+		return view('pages.password_recovery.form');
 	}
 
+	public function sendEmail(Request $request)
+	{
+		$validation_rules = array(
+			'email'                 => 'required|email',  
+			'g-recaptcha-response'  => 'required|captcha'
+		);
+		$validator = Validator::make(Input::all(), $validation_rules);
+		if ($validator->fails())
+		{
+			return Redirect::to('password-recovery')->withErrors($validator)->withInput();;
+		}
+		$email = trim(Input::get('email'));
+		// find user with matching email address.
+		$matching_user = User::where('email', '=', $email)->first();
+		if ( !$matching_user ) {
+			return view('pages.password_recovery.unmatched_email');
+		}
+		
+		mail($email, 'Password Recovery', 'Your password has been reset.');
+		return view('pages.password_recovery.email_sent');
+	}
 }
---
+++ app/resources/views/pages/password_recovery/unmatched_email.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.default')
+@section('content')
+
+<div class="password-recovery">
+	<h1>Password Recovery</h1>
+	
+	<div class="form-box">
+		There is no user with that email address.
+	</div>
+</div>
+
+@stop
