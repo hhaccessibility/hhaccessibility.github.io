ec5ed0c4ef73bdbc44c3077d2e41857ba9c11697
jgreig <josh.greig@gmail.com>
2017-04-01 17:20:42 -0400

    added question_category_id to ReviewComment fillable properties
    This was part of making review comments question-category-specific.  The
    field was added to the review_comment table about a month ago but wasn't
    made updatable through ReviewComment instances.

--- app/app/ReviewComment.php
+++ app/app/ReviewComment.php
@@ -6,7 +6,7 @@ use Eloquent;
 class ReviewComment extends Eloquent
 {
     protected $fillable = [
-        'content', 'answered_by_user_id', 'location_id'
+        'content', 'answered_by_user_id', 'question_category_id', 'location_id'
     ];
 	
 	protected $table = 'review_comment';
