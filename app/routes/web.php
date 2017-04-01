d505ed9addd80db237ecaec61c53996c23f8d11a
jgreig <josh.greig@gmail.com>
2017-04-01 16:59:43 -0400

    app: added submit method and route
    This will be useful for committing rating answers.  In other words, moving
    the rating answers for a specific location from the session memory to the
    database.

--- app/routes/web.php
+++ app/routes/web.php
@@ -42,6 +42,7 @@ Route::get('location-report/{location_id}/{rating_system}/{question_category_id}
 Route::get('location-rating/{location_id}', 'LocationRatingController@show');
 Route::get('location-rating/{location_id}/{question_category_id}', 'LocationRatingController@show');
 Route::get('reviewed-locations', 'LocationRatingController@reviewedLocations');
+Route::post('location-rating-submit', 'LocationRatingController@submit');
 Route::put('location-rating/answer', 'LocationRatingController@setAnswer');
 Route::delete('location-rating/answer', 'LocationRatingController@removeAnswer');
 Route::put('location-rating/comment', 'LocationRatingController@setComment');
