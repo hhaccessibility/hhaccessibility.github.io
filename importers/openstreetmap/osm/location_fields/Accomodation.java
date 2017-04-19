f002e263622426f5eb877fbc3138e8df787eda8c
jgreig <josh.greig@gmail.com>
2017-04-19 06:08:25 -0400

    importers: osm: improved some of the category detection for Education and Accommodation

--- importers/openstreetmap/osm/location_fields/Accomodation.java
+++ importers/openstreetmap/osm/location_fields/Accomodation.java
@@ -14,7 +14,12 @@ public class Accomodation extends LocationField
 	{
 	   String barrier = location.getValueFor("barrier");
 	   String tourism = location.getValueFor("tourism");
-		return tourism.equals("hotel") || tourism.equals("motel") || tourism.equals("hostel")
-		|| tourism.equals("bnb");		
+	   String name = location.getValueFor("name").trim();
+		return tourism.equals("hotel") || tourism.equals("motel")
+		|| tourism.equals("inn")
+		|| tourism.equals("hostel")
+		|| tourism.equals("bnb") || name.contains("Motor Inn")
+		|| name.contains("Inn Suite") || name.contains("Hotel") 
+		|| name.contains("Motel") || name.contains("Hostel");
 	}
 }
