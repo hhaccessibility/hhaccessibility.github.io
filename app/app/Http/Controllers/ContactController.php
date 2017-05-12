192fc13c8ff19b6486af8f5ea32775c6daf3e2bd
Josh Greig <josh.greig@gmail.com>
2017-05-12 01:35:54 +0000

    move smtp header from to php code

--- app/app/Http/Controllers/ContactController.php
+++ app/app/Http/Controllers/ContactController.php
@@ -32,7 +32,8 @@ class ContactController extends Controller {
 		$to_email = 'accesslocator@gmail.com';
 		$subject = 'app accesslocator com contact message';
 		$body = "Hello,\r\nYou have a message from: ".$email.":\r\n\r\n".$message;
-		$headers = "Content-Type: text/plain; charset=\"utf-8\"\r\n";
+		$headers = "From: noreply@accesslocator.com\r\n";
+		$headers .= "Content-Type: text/plain; charset=\"utf-8\"\r\n";
 		mail($to_email, $subject, $body, $headers);
 		return view('pages.contact.message-sent');
 	}
