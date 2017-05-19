55d7d69e6557e88159bef1c4a374ad86d5abf779
jgreig <josh.greig@gmail.com>
2017-05-19 00:12:44 -0400

    location_search_table.js: refactored and made error cases more visible when ajax request fails
    Related to #260

--- app/public/js/location_search_table.js
+++ app/public/js/location_search_table.js
@@ -1,31 +1,42 @@
-function bindUpdateRadius() {
-	$("#updateRadius").click(onclickUpdateButtonEvent);
-	function onclickUpdateButtonEvent(event){
-		var distNumber = parseFloat($("#distance").val());
-		if (!distNumber || distNumber<=0 ) {
-			$("dev.disform").addClass("has-error");
-			$("#distance").val("");
-			return;
+function onUpdateRadiusError()
+{
+	$("dev.disform").addClass("has-error");
+	$("#distance").val("");
+}
+
+function updateRadiusClicked()
+{
+	var distNumber = parseFloat($("#distance").val());
+	if (!distNumber || distNumber<=0 ) {
+		onUpdateRadiusError();
+		return;
+	}
+	$.ajax(
+		{
+			url: "/api/set-search-radius",
+			type: 'post',
+			headers: {
+				'X-CSRF-Token': $("#_token").val()
+			},
+			data: {
+				'distance': distNumber,
+				'_token': $("#_token").val()
+			},
+			fail: function() {
+				console.error('Something failed in ajax request');
+				onUpdateRadiusError();
+			},
+			success: refreshPage
 		}
-		$.ajax(
-			{
-				url: "/api/set-search-radius",
-				type: 'post',
-				headers: {
-					'X-CSRF-Token': $("#_token").val()
-				},
-				data: {
-					'distance': distNumber,
-					'_token': $("#_token").val()
-				},
-				success: refreshPage
-			}
-		);
-		event.preventDefault();
-	}
-	function refreshPage() {
-		location.reload();
-	}
+	);
+}
+
+function refreshPage() {
+	location.reload();
+}
+
+function bindUpdateRadius() {
+	$("#updateRadius").click(updateRadiusClicked);
 }
 
 $(document).ready(function() {
