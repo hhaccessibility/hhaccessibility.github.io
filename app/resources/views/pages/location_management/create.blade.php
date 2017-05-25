4107101bb2bedc07c0ef8c419d3d7401e818e47d
jgreig <josh.greig@gmail.com>
2017-05-24 22:20:07 -0400

    added location create view
    This is a start to a location creation feature intended for internal
    users.

---
+++ app/resources/views/pages/location_management/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default')
+@section('content')
+<div class="location-management">
+	<h1>Create New Location</h1>
+
+	<form method="post" action="/location-modify">
+		@include('pages.location_management.location_form',
+					array(
+						'location' => $location,
+						'data_sources' => $data_sources,
+						'location_tags' => $location_tags
+					))
+	</form>
+	
+</div>
+@stop
