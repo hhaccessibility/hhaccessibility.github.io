066d587e191481c8714f38f55a906d4de1013747
Bruce <brucelau.email@gmail.com>
2017-05-24 22:15:53 -0400

    Profile save prompt: add shared variable toggle isProfilechanged. related to issue #349

--- app/public/js/profile_save_button.js
+++ app/public/js/profile_save_button.js
@@ -1,12 +1,14 @@
+var isProfileChanged = false;
 $(document).ready(function() {
 	$("#profileForm").change(formChange);
 	var DefaultFormValue = $("#profileForm").serialize();
-
 	function formChange() {
 		if ( $("#profileForm").serialize() === DefaultFormValue) {
 			$("#submitButton").prop("disabled", true);
+			isProfileChanged = false;
 		} else {
 			$("#submitButton").prop("disabled", false);
+			isProfileChanged = true;
 		}
 	}
 });
