276617f02513d22043df704d1a8aadda283e922f
Bruce <brucelau.email@gmail.com>
2017-05-27 23:01:46 -0400

    Forget password: add input hidden to reset form. related to issue #204

--- app/resources/views/pages/password_recovery/reset_password.blade.php
+++ app/resources/views/pages/password_recovery/reset_password.blade.php
@@ -5,6 +5,8 @@
 
 	<div class="password-change-form">
 		<form method="post" action="/password-recovery/reset-password">
+			<input type="hidden" name="user_email" value="{{ $user_email }}">
+			<input type="hidden" name="password_recovery_token" value="{{ $password_recovery_token }}">
 			{!! csrf_field() !!}
 			@include('pages.validation_messages', array('errors'=>$errors))
 			<div class="row">
