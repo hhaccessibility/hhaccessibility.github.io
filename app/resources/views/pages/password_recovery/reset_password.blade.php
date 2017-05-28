0f0f8dbabc0287897ded15d68a4664f45db7f217
Bruce <brucelau.email@gmail.com>
2017-05-27 23:02:13 -0400

    Forget password: remove bootstrap grid system. related to issue #204

--- app/resources/views/pages/password_recovery/reset_password.blade.php
+++ app/resources/views/pages/password_recovery/reset_password.blade.php
@@ -9,20 +9,18 @@
 			<input type="hidden" name="password_recovery_token" value="{{ $password_recovery_token }}">
 			{!! csrf_field() !!}
 			@include('pages.validation_messages', array('errors'=>$errors))
-			<div class="row">
-				<div class="col-xs-12">
-					<input class="clean" type="password" name="new_password"
-						placeholder="New Password"
-						value="">
-				</div>
-				<div class="col-xs-12">
-					<input class="clean" type="password" name="password_confirm"
-						placeholder="Password Confirmation"
-						value="">
-				</div>
-				<div class="col-xs-12">
-					<input type="submit" class="clean" value="Update Password">
-				</div>
+			<div>
+				<input class="clean" type="password" name="new_password"
+					placeholder="New Password"
+					value="">
+			</div>
+			<div>
+				<input class="clean" type="password" name="password_confirm"
+					placeholder="Password Confirmation"
+					value="">
+			</div>
+			<div>
+				<input type="submit" class="clean" value="Update Password">
 			</div>
 		</form>
 	</div>
