41c2adc71fe4b15f1707eb0ae89b3164a1fc579e
jgreig <josh.greig@gmail.com>
2017-07-25 00:02:57 -0400

    importers: uwindsor.ca: added a few category columns to the generated CSV
    Added is_restaurant, is_education, is_transportation, is_accommodation...
    columns to the uwindsor.ca campus importer.
    
    Also, added README.md file for the importer tool.
    
    Related to #381

--- importers/uwindsor.ca/download_xml.py
+++ importers/uwindsor.ca/download_xml.py
@@ -6,7 +6,40 @@ import urllib.request
 # Decides if we want to collect information on the specified type of location
 # Types such as 'aEntrance' aren't of interest since they're parts of a larger building that we'd want to rate as a whole.
 def is_location_type_of_interest(type):
-	return type not in ['aEntrance', 'ebike', 'ephone', 'smoking']
+	return type not in ['aEntrance', 'ebike', 'ephone', 'smoking',
+		'parkingPay', 'parkingStaff', 'parkingStudent']
+
+		
+def is_restaurant(location):
+	return location['type'] in ['restaurant', 'eatingSite']
+
+
+def is_transportation(location):
+	return location['type'] in ['aTransit', 'parkingPay', 'parkingStaff', 'parkingStudent', 'ebike']
+	
+
+def is_education(location):
+	if location['name'] in ['Forge Fitness Centre', 'Graduate Student Society',
+	'Faculty Association (Kerr House)', 'Energy Conversion Centre',
+	'Computer Centre - IT Services', 'Community Legal Aid']:
+		return False
+
+	return location['type'] in ['ubuilding']
+
+	
+def is_financial(location):
+	return location['name'] in ['Central Receiving', 'Student Awards and Financial Aid']
+
+
+def is_accommodation(location):
+	return (location['type'] == 'ubuilding' and
+		location['name'] in ['Vanier Hall', 'Electa Hall', 'Macdonald Hall',
+		'Alumni Hall and Conference Centre', 'Cartier Hall', 'Laurier Hall',
+		'Union House'])
+
+
+def is_sports(location):
+	return location['name'] in ['Forge Fitness Centre', 'Human Kinetics', 'Stadium'])
 
 
 # Returns a list of locations extracted from the specified XML file.
@@ -27,15 +60,23 @@ def get_locations(file_name):
 			# if only part of a URL given, include the full path.
 			if not url.startswith('http'):
 				url = 'http://web2.uwindsor.ca/pac/campusmap/' + url
+			
 			# encoding and decoding to utf-8 was required in order to avoid errors during csv creation
-			locations.append({
+			location = {
 				'name': i.get('name').encode("utf-8"),
 				'latitude': i.get('lat').decode("utf-8"),
 				'longitude': i.get('lng').decode("utf-8"),
 				'type': type,
 				'description': i.get('mtext').encode("utf-8"),
 				'url': url
-			})
+			}
+			location['is_restaurant'] = is_restaurant(location)
+			location['is_education'] = is_education(location)
+			location['is_transportation'] = is_transportation(location)
+			location['is_financial'] = is_financial(location)
+			location['is_accommodation'] = is_accommodation(location)
+			location['is_sports'] = is_sports(location)
+			locations.append(location)
 
 	return locations
 
--- importers/uwindsor.ca/generate_csv.py
+++ importers/uwindsor.ca/generate_csv.py
@@ -5,7 +5,9 @@ from download_xml import get_locations
 def generate_csv():
 	locations = get_locations('building_markers.xml')
 	filename = 'building_accessibility_info.csv'
-	keys = ['name', 'latitude', 'longitude', 'type', 'url', 'description']
+	keys = ['name', 'latitude', 'longitude', 'type', 'url', 'description',
+		'is_education', 'is_restaurant', 'is_transportation', 'is_financial',
+		'is_accommodation', 'is_sports']
 	with open(filename, 'w') as csv_file:
 		csv_writer = csv.DictWriter(csv_file, fieldnames=keys)
 		csv_writer.writeheader()
