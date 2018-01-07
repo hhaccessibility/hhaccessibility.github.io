029e58d7cd097c9277afb3b0c68807703d4b2af4
jgreig <josh.greig@gmail.com>
2018-01-07 13:51:07 -0500

    app: added comment to clarify purpose of introduction view
    Done while working on #463

--- app/resources/views/pages/location_rating/introduction.blade.php
+++ app/resources/views/pages/location_rating/introduction.blade.php
@@ -1,3 +1,4 @@
+{{-- introduction.blade.php is rarely used. It is used by rate.blade.php only if the question category is null. --}}
 <p>
 To rate this location, select one of the options on the left.
 </p>
