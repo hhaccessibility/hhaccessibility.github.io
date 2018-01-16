d87f55b6f70e0bdcf5e15db1accf8a89325a9de4
jgreig <josh.greig@gmail.com>
2018-01-15 21:48:17 -0500

    app: reworded title of My Added Locations page
    This is in response to feedback from Blandine on #454.
    
    Blandine commented, "Change "Locations Added By Me" to "Added Locations"".

--- app/resources/views/pages/location_management/locations_added_by_me.blade.php
+++ app/resources/views/pages/location_management/locations_added_by_me.blade.php
@@ -3,10 +3,10 @@
 <div class="locations-added-by-me">
 	<div class="text-center">
 		<a class="add-create" href="/add-location" title="Add New Location">+</a>
-		<h1>Locations Added By Me ({{ count($locations) }})</h1>
+		<h1>Added Locations ({{ count($locations) }})</h1>
 	</div>
 	@if ( count($locations) === 0 )
-		<p>No locations have been added yet.</p>
+		<p class="text-center">No locations have been added yet.</p>
 	@else
 	<div class="row column-titles">
 		<div class="col-xs-4">
