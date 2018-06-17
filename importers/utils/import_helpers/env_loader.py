909f42dd088a329b941153103d10ad2e287ca1dd
jgreig <josh.greig@gmail.com>
2018-06-17 16:51:29 -0400

    importers: utils: removed comments from .env file when converting it to a dict

--- importers/utils/import_helpers/env_loader.py
+++ importers/utils/import_helpers/env_loader.py
@@ -1,8 +1,18 @@
+# Inline comments in a .env start with a '#' similar to Python.
+# More discussion at: https://stackoverflow.com/questions/32368016/how-to-comment-in-laravel-env-file
+def remove_inline_comment(line):
+	if '#' in line:
+		return line[:line.find('#')]
+	else:
+		return line
+
+
 def get_env_data():
 	env_filename = '../../app/.env'
 	with open(env_filename, 'r') as env_f:
 		content = env_f.readlines()
-		lines = [line.strip() for line in content if '=' in line]
+		lines = [remove_inline_comment(line).strip() for line in content]
+		lines = [line for line in lines if '=' in line]
 		key_values = {}
 		for line in lines:
 			index = line.find('=')
