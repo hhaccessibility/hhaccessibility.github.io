f505821eb0437ac6fd4cd4b8d6d19c20f023d312
jgreig <josh.greig@gmail.com>
2018-06-20 22:41:32 -0400

    app: fixed data type for raw_data field
    Related to #411

--- app/database/migrations/2018_06_17_002542_create_image_table.php
+++ app/database/migrations/2018_06_17_002542_create_image_table.php
@@ -3,14 +3,10 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateImageTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('image', function (Blueprint $table) {
@@ -21,8 +17,14 @@ class CreateImageTable extends Migration
             $table->foreign('uploader_user_id')->references('id')->on('user');
             $table->string('location_id', 36)->nullable(false);
             $table->foreign('location_id')->references('id')->on('location');
-            $table->binary('raw_data', 16777215);
         });
+        /*
+        Unfortunately, if we can't make a 16MB blob type with Laravel's "bianry" type.
+        Instead, we use raw SQL.
+
+        To learn more, see: https://stackoverflow.com/questions/20089652/mediumblob-in-laravel-database-schema
+        */
+        DB::statement("ALTER TABLE image ADD raw_data MEDIUMBLOB");
     }
 
     /**
