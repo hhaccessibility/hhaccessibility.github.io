bd5c426d6736ffb3181a9c70b95623ccd82fca03
jgreig <josh.greig@gmail.com>
2018-06-29 17:15:58 -0400

    app: HomeTest.php fixed
    Related to #567

--- app/tests/HomeTest.php
+++ app/tests/HomeTest.php
@@ -5,7 +5,8 @@ class HomeTest extends TestCase
     public function testGet()
     {
 		$content = $this->get('/')->seeStatusCode(200)->response->getContent();
-		$this->assertTrue(strpos($content, 'Sign in / Sign up') !== false);
+		$this->assertTrue(strpos($content, 'Sign in') !== false);
+		$this->assertTrue(strpos($content, 'Sign up') !== false);
 		$this->assertTrue(strpos($content, 'Search by keyword(s)') !== false);
     }
 }
