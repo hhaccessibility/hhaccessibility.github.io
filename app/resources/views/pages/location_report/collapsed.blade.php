18775c7debeb36e9346acf9071f267a8e1ada76e
parth7676 <parth7676@gmail.com>
2018-07-13 15:25:33 -0400

    Added phone number for location.

--- app/resources/views/pages/location_report/collapsed.blade.php
+++ app/resources/views/pages/location_report/collapsed.blade.php
@@ -23,8 +23,13 @@
 		'location' => $location))
 	<div class="basic-info">
 		<div class="addresses">
-			<address>{{ $location->address }}</address>
-			<a href="{{ $location->getExternalWebURL() }}">{{ $location->getExternalWebURL() }}</a>
+			<address>
+				<em class="fa fa-map-marker"></em>
+				{{ $location->address }}<br>
+				@if($location->phone_number)<p><em class="fa fa-phone"></em>
+				<a href="tel: {{ $location->phone_number }}">{{ $location->phone_number }}</a></p>@endif
+				<a href="{{ $location->getExternalWebURL() }}">{{ $location->getExternalWebURL() }}</a>
+			</address>
 		</div>
 		<div class="location-tags text-right">
 			<div class="location-tags">
