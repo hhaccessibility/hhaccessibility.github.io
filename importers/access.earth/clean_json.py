0354a7ceb6995be984714a6ea4c53291013eb0a1
Matthew Farias <ezioblade64@gmail.com>
2018-07-19 13:29:01 -0400

    Removed convinience stores as an association, and now checks for words 'school, university, college' in name to add to Education

--- importers/access.earth/clean_json.py
+++ importers/access.earth/clean_json.py
@@ -7,6 +7,9 @@ table = {}
 keys=['name','address','postalCode','lat','lng','is_Accom','is_Arts','is_Asso','is_Edu','is_Finan',
 	'is_Healthcare','is_Parks','is_Public','is_Resturant','is_Shop','is_Sports','is_Trans']
 
+
+educationWords=['school','university','college']
+
 def addTable(line):
 	seperate = line.split(',')
 	table[seperate[0]]=seperate[1:]
@@ -31,6 +34,10 @@ def clean(vals):
 	vals['lng']=location['lng']
 	vals['is_Accom'] = vals['is_Arts'] = vals['is_Asso'] = vals['is_Edu'] = vals['is_Finan'] = vals['is_Healthcare'] = False
 	vals['is_Parks'] = vals['is_Public'] = vals['is_Resturant'] = vals['is_Shop'] = vals['is_Sports'] = vals['is_Trans'] = False
+
+	for i in educationWords:
+		if i in name.lower():
+			vals['is_Edu'] = True
 
 	if len(cats)>0:
 		shortName=cats[0]['shortName']
