8120185fbb1a738a383019328d9638a884ddcf45
jgreig <josh.greig@gmail.com>
2018-07-20 19:00:58 -0400

    app: adjusted colours of password recovery email template to match logo

--- app/resources/views/mails/password_recovery.blade.php
+++ app/resources/views/mails/password_recovery.blade.php
@@ -146,7 +146,7 @@
                                 <td bgcolor="#ffffff" align="center" style="padding: 20px 30px 60px 30px;">
                                     <table border="0" cellspacing="0" cellpadding="0">
                                         <tr>
-                                            <td align="center" style="border-radius: 3px;" bgcolor="#7c72dc"><a href="{{$recoveryLink}}" target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #7c72dc; display: inline-block;">Reset Password</a></td>
+                                            <td align="center" style="border-radius: 3px;" bgcolor="#2e1d66"><a href="{{$recoveryLink}}" target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px;display: inline-block;">Reset Password</a></td>
                                         </tr>
                                     </table>
                                 </td>
@@ -188,7 +188,7 @@
             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;" >
                 <!-- HEADLINE -->
                 <tr>
-                    <td bgcolor="#C6C2ED" align="center" style="padding: 30px 30px 30px 30px; border-radius: 4px 4px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;" >
+                    <td bgcolor="#C6C2ED" align="center" style="padding: 30px 30px 30px 30px; border-radius: 4px 4px 4px 4px; color: #949ea7; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;" >
                         <h2 style="font-size: 20px; font-weight: 400; color: #111111; margin: 0;">Need more help?</h2>
                         <p style="margin: 0;"><a href="https://app.accesslocator.com/contact" target="_blank" style="color: #7c72dc;">Contact Us</a></p>
                     </td>
