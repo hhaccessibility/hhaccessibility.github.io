ca59854fa54f94d5ec8e08333baeebfd9815224c
Matthew Farias <ezioblade64@gmail.com>
2018-07-23 11:01:08 -0400

    #648, now lets users resend password verification link

--- app/resources/views/pages/password_recovery/email_sent.blade.php
+++ app/resources/views/pages/password_recovery/email_sent.blade.php
@@ -7,6 +7,16 @@
 	<div class="form-box">
 		Check your email in a few minutes.
 		You'll receive a link to reset your password.
+		If you do not receive an email after 5 minutes, you can resend the email
+		<form method="post" action="/user/password-recovery">
+			{!! csrf_field() !!}
+			@include('pages.validation_messages', array('errors'=>$errors))					
+			<input class="clean" type="email" name="email" placeholder="Your Email">
+			<div>
+				{!! app('captcha')->display(); !!}
+			</div>
+			<input class="clean" type="submit" value="Send Password Recovery Email">
+		</form>
 	</div>
 </div>
 
