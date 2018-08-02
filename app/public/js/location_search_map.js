4461554f7acba5b9baa399d74fc54f1fd11124f3
Matthew Farias <ezioblade64@gmail.com>
2018-08-02 11:34:48 -0400

    app: drag works perfectly even on localhost now
    Related to #657

--- app/public/js/location_search_map.js
+++ app/public/js/location_search_map.js
@@ -121,7 +121,7 @@ function initMap() {
 
 	map.addListener('dragend', function() {
 		saveSearchLocationWithOptionalAddress(map.getCenter());
-		location.reload();
+		circle.setCenter(map.getCenter());
 	});
 
 	var lastRight = null;
