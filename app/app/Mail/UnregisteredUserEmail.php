9f9770957a4c17cba65d2a1ad91b24e83cddb10b
hari <hari@sarct.in>
2018-12-27 22:30:09 +0530

    After coding style update in UnregisterUserEmail.php file

--- app/app/Mail/UnregisteredUserEmail.php
+++ app/app/Mail/UnregisteredUserEmail.php
@@ -22,7 +22,7 @@ class UnregisteredUserEmail extends Mailable
      * @param $recoveryLink
      * @param null $fromEmail
      */
-    public function __construct($toEmail,  $fromEmail = null)
+    public function __construct($toEmail, $fromEmail = null)
     {
         $this->toEmail = $toEmail;
         $this->fromEmail = $fromEmail;
