2a6291314146a5972fae8e781819b4739a3c433b
jgreig <josh.greig@gmail.com>
2019-01-07 00:19:46 -0500

    app: added a feature for deleting an image in the location image slideshow
    Related to #411

--- app/app/Http/Controllers/LocationImageController.php
+++ app/app/Http/Controllers/LocationImageController.php
@@ -1,7 +1,10 @@
 <?php namespace App\Http\Controllers;
 
 use App\Image;
+use App\Role;
+use App\BaseUser;
 use Illuminate\Routing\Controller;
+use Illuminate\Auth\AuthenticationException;
 use Response;
 use DB;
 
@@ -9,7 +12,7 @@ class LocationImageController extends Controller
 {
     public function getImagesFor(string $location_id)
     {
-        return Image::where('location_id', '=', $location_id)
+        $result = Image::where('location_id', '=', $location_id)
             ->join('user', 'user.id', '=', 'image.uploader_user_id')
             ->select(
                 'image.id',
@@ -18,6 +21,20 @@ class LocationImageController extends Controller
                 DB::raw('concat(user.first_name, \' \', user.last_name) as uploader_name')
             )
             ->get();
+        $all_can_be_deleted = false;
+        $user_id = null;
+        if (BaseUser::isSignedIn()) {
+            $user = BaseUser::getDbUser();
+            $user_id = $user->id;
+            if ($user->hasRole(Role::INTERNAL)) {
+                $all_can_be_deleted = true;
+            }
+        }
+        foreach ($result as $image) {
+            $image->can_be_deleted = $all_can_be_deleted || $image->uploader_user_id === $user_id;
+            unset($image->uploader_user_id);
+        }
+        return $result;
     }
 
     public function getImage(string $image_id)
@@ -32,5 +49,28 @@ class LocationImageController extends Controller
         $response = Response::make($image->raw_data, 200);
         $response->header('Content-Type', 'image/jpeg');
         return $response;
+    }
+
+    public function deleteImage(string $image_id)
+    {
+        if (!BaseUser::isSignedIn()) {
+            throw new AuthenticationException('Must be signed in to delete an image');
+        }
+        $user = BaseUser::getDbUser();
+        $image = Image::find($image_id);
+        if (!$image) {
+            return Response::json([
+                    'message'   =>  'Image not found with specified id'
+                ], 404);
+        }
+        if (!$user->hasRole(Role::INTERNAL) && $image->uploader_user_id !== $user->id) {
+            return Response::json([
+                    'message'   =>  'Only the uploader of an image can delete it'
+                ], 403);
+        }
+        $image->delete();
+        return Response::json([
+            'message'   =>  'Image deleted'
+        ], 200);
     }
 }
--- app/public/js/location_image_slideshow.js
+++ app/public/js/location_image_slideshow.js
@@ -4,7 +4,7 @@ Makes image slideshow available for location report
 $(document).ready(function() {
 	var images;
 	var current_image_index = 0;
-	
+
 	function getImageURL(image_id) {
 		return '/location/image/' + image_id;
 	}
@@ -40,28 +40,76 @@ $(document).ready(function() {
 		$image.css({
 			backgroundImage: 'url("' + getImageURL(images[current_image_index].id) + '")'
 		});
+		if (images[current_image_index].can_be_deleted) {
+			$('.slideshow').addClass('can-be-deleted');
+		}
+		else {
+			$('.slideshow').removeClass('can-be-deleted');
+		}
 	}
-	
-	function nextClicked() {
+
+	function nextClicked(event) {
+		if (!event) {
+			event = window.event;
+		}
 		current_image_index = (current_image_index + 1) % images.length;
 		updateBackgroundImage();
 		event.stopImmediatePropagation();
 	}
 
 	function processArrowKey(event) {
+		if (!event) {
+			event = window.event;
+		}
 		if (event.keyCode === 39) {
-			// right key
-			nextClicked();
+			// right arrow key
+			nextClicked(event);
 		}
 		else if (event.keyCode === 37) {
-			previousClicked();
+			// left arrow key
+			previousClicked(event);
+		}
+		else if (event.keyCode === 46) {
+			deleteClicked(event);
 		}
 	}
 
-	function previousClicked() {
+	function previousClicked(event) {
+		if (!event) {
+			event = window.event;
+		}
 		current_image_index = (current_image_index + images.length - 1) % images.length;
 		updateBackgroundImage();
 		event.stopImmediatePropagation();
+	}
+
+	function deleteClicked(event) {
+		if (!event) {
+			event = window.event;
+		}
+		event.stopImmediatePropagation();
+		if (!images[current_image_index].can_be_deleted) {
+			return; // Do nothing because we're not allowed to.
+		}
+		var image_id = images[current_image_index].id;
+		deleteImage(image_id).then(function() {
+			// Remove the deleted image from the images Array.
+			images = images.filter(function(image) {
+				return image.id !== image_id;
+			});
+			// If no images remain, hide the slideshow and the image button.
+			if (images.length === 0) {
+				hideSlideShowButton();
+				hideSlideshow();
+			}
+			else {
+				// Make sure current_image_index stays in a valid Array index.
+				if (current_image_index >= images.length) {
+					current_image_index = 0;
+				}
+				updateBackgroundImage();
+			}
+		});
 	}
 
 	function showSlideshow() {
@@ -69,14 +117,16 @@ $(document).ready(function() {
 		$slideshow.click(hideSlideshow);
 		var $dialog = $('<div class="slideshow-dialog"></div>');
 		var $image = $('<div class="image"></div>');
-		updateBackgroundImage($image);
 		var $next = $('<a class="next" title="Next"></a>').click(nextClicked);
 		$dialog.append($next);
 		var $previous = $('<a class="previous" title="Previous"></a>').click(previousClicked);
 		$dialog.append($previous);
+		var $delete = $('<a class="delete" title="Delete image"><em class="fa fa-trash"></em></a>').click(deleteClicked);
 		$dialog.append($image);
+		$dialog.append($delete);
 		$slideshow.append($dialog);
 		$('body').append($slideshow);
+		updateBackgroundImage($image);
 		$(document).on('keydown', processArrowKey);
 	}
 
@@ -87,6 +137,11 @@ $(document).ready(function() {
 			$images_button.addClass('has-images');
 			$images_button.find('a').click(showSlideshow);
 		}
+	}
+
+	function hideSlideShowButton() {
+		var $images_button = $('.images-button');
+		$images_button.removeClass('has-images');
 	}
 
 	function preloadImages() {
@@ -102,6 +157,19 @@ $(document).ready(function() {
 			'error': function() {
 				console.error('Failed to get images.');
 			}
+		});
+	}
+	
+	function deleteImage(image_id) {
+		return $.ajax({
+				'url': '/api/location/image/' + image_id,
+				'method': 'DELETE',
+				'data': {
+					'_token': csrfToken,
+				},
+				'error': function( jqXHR, textStatus, errorThrown) {
+					console.error('Failed to delete image ' + image_id + ' textStatus = ' + textStatus + ', errorThrown = ' + errorThrown + ', jqXHR = ', jqXHR);
+				}
 		});
 	}
 
