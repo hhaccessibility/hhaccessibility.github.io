672fae5fada145d9e3fca554776e225c9861d30e
Josh Greig <josh.greig@gmail.com>
2019-11-16 12:45:58 -0500

    password_recovery: renamed Account Recovery to Password Recovery
    Related to issue #744

--- app/resources/views/pages/password_recovery/unmatched_token.blade.php
+++ app/resources/views/pages/password_recovery/unmatched_token.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <div class="password-recovery">
-	<h1>Password Recovery</h1>
+	<h1>Forgotten Password</h1>
 
 	<div class="form-box">
 		Your password recovery token does not match our record.
